516d0ec08e68e35cb43d25a0a4db9259147efead
link0233 <054656-106144@stu.mlc.edu.tw>
2021-10-19 17:49:31 +0800

    完成按下後的事件

--- covor/strat_button.py
+++ covor/strat_button.py
@@ -12,7 +12,9 @@ class start_button:
         if abs(key['xy'][0]-self.xy[0])<100 and abs(key['xy'][1]-self.xy[1])<20:
             self.btn={'bk':self.canvas.create_rectangle(220,200,420,240,fill='#3b3b3b')
                 ,'text':self.canvas.create_text(320,220,text='繼續遊戲',font=('Arial',20),fill='#000000')
-            } 
+            }
+            if key['button1']==1:
+                return 1
         else:
             self.btn={'bk':self.canvas.create_rectangle(220,200,420,240,fill='White')
                 ,'text':self.canvas.create_text(320,220,text='繼續遊戲',font=('Arial',20),fill='#000000')
