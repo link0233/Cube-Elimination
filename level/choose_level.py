c9e1e39c17e2bb3062d101a0d2c3ba37b177a86f
link0233 <054656-106144@stu.mlc.edu.tw>
2021-10-24 07:14:53 +0800

    以.csv存檔

--- level/choose_level.py
+++ level/choose_level.py
@@ -6,7 +6,7 @@ class choose:
     def level_choose_start(self,level,xy):
         self.items.append({
             'bk':self.cavnas.create_rectangle(xy[0]-27,xy[1]-27,xy[0]+27,xy[1]+27,fill='#d69fd3'),
-            'number':self.cavnas.create_text(xy[0],xy[1],text=str(level['level']),font=('Arial',15),fill='White'),
-            'score':level['score'],
+            'number':self.cavnas.create_text(xy[0],xy[1]-10,text=level['level'],font=('Arial',10),fill='White'),
+            'score':int(level['score']),
             'level':level    
             })
--- level/choose_level_main.py
+++ level/choose_level_main.py
@@ -4,7 +4,31 @@ class choose_level_main:
     def __init__(self,canvas):
         self.canvas=canvas
         self.choose=choose(canvas)
+        self.text=''
+        with open('./level/level.csv','r') as f:
+            for i in f:
+                self.text+=i
+        self.text=self.text.split('\n')
+        self.textt=[]
+        for text in self.text:
+            self.textt.append(text.split(','))
+        self.text=[]
+        for text in self.textt:
+            self.text.append({
+                'level':text[0],
+                'score':text[1]
+            })
+        print(self.text)
 
     def choose_level_start(self):
         self.canvas.create_rectangle(0,0,640,480,fill='skyblue')
-        self.choose.level_choose_start({'level':1,'score':13},[100,100])
+        self.xy=[0,37]
+        self.t=0
+        for text in self.text:
+            self.t+=1   
+            if self.t>10:
+                self.t=0
+                self.xy[0]=0
+                self.xy[1]+=37
+            self.xy[0]+=37
+            self.choose.level_choose_start(text,self.xy)
